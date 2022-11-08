a70e9e63825559adf8ec62fd8c8a762103c9b68a
Edgar <edgarsargsyan@hotmail.com>
2022-11-08 16:30:03 -0600

    Render the Category-container div

--- src/components/category-item/category-item.component.js
+++ src/components/category-item/category-item.component.js
@@ -1,6 +1,20 @@
-import './categories-item.styles.scss'
+import './category-item.styles.scss'
 
-const CategoryItem = () => {
+const CategoryItem = ({ category }) => {
+  const { imageUrl, title} = category;
+  return (
+    <div className='category-container'>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }} />
+      <div className='category-body-container'>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  )
 
 
 }
