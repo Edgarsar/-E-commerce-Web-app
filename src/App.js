ba123e602fc7719c53ae3807d14ad28a43cb892c
Edgar <edgarsargsyan@hotmail.com>
2022-11-08 15:41:00 -0600

    fixing brackets

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-
-
 const App = () => {
-
   const categories = [
     {
-      is: 1,
-      title: "Hats",
+      id: 1,
+      title: 'Hats',
     },
     {
-      is: 2,
-      title: "Jackets",
+      id: 2,
+      title: 'Jackets',
     },
     {
-      is: 3,
-      title: "Sneakers",
+      id: 3,
+      title: 'Sneakers',
     },
     {
-      is: 4,
-      title: "Womens",
+      id: 4,
+      title: 'Womens',
     },
     {
-      is: 5,
-      title: "Mens",
+      id: 5,
+      title: 'Mens',
     },
   ];
 
@@ -29,15 +26,13 @@ const App = () => {
     <div className='categories-container'>
       {categories.map(({ title }) => (
         <div className='category-container'>
-          {/* <img /> */}
+          <div className='background-image' />
           <div className='category-body-container'>
             <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
         </div>
       ))}
-
-
     </div>
   );
 };
