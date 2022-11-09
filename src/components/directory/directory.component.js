c36acc1d68741b15aba929ddf89d49da136abcfe
Edgar <edgarsargsyan@hotmail.com>
2022-11-09 17:43:23 -0600

    Change the className from categories-container tio directory-container

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -1,10 +1,10 @@
 import CategoryItem from '../category-item/category-item.component';
 
 
-const Directory = ({categories}) => {
+const Directory = ({ categories }) => {
 
   return (
-    <div className='categories-container'>
+    <div className='directory-container'>
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
