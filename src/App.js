38415e206dd7768fd96e3a97c7c1550248a8c8ea
Edgar <edgarsargsyan@hotmail.com>
2022-11-09 19:24:07 -0600

    implement Shop component just for testing the routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
-const Navigation = () => {
+const Shop = () => {
   return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
+    <h1>I am the Shop page</h1>
+  )
 }
-
 const App = () => {
 
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
   );
