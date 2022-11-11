7ecf3583000b6d2610b1ef40e59a90b1b5e8ca86
Edgar <edgarsargsyan@hotmail.com>
2022-11-10 19:18:47 -0600

    Initialize a logGoogleUser function for signing up with google popup

--- src/routes/sign-in/sign-in.component.js
+++ src/routes/sign-in/sign-in.component.js
@@ -1,8 +1,19 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
 
 
-const SignIn = ()=>{
   return (
-    <h1>This is a Sign In page</h1>
+    <div>
+      <h1>This is a Sign In page</h1>
+      <button onClick={logGoogleUser}>
+        Sign in with google Popup
+      </button>
+    </div>
   );
 };
 
