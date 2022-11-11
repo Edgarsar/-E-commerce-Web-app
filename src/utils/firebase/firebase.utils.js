5231958bc91d61ce3cebebe4c3833a68b132558a
Edgar <edgarsargsyan@hotmail.com>
2022-11-10 18:53:27 -0600

    Setup firebase config

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVHUUyJoZ8uh3ZVFB7T15qbVNSEQDEU80",
+  authDomain: "capstone-db-54ca2.firebaseapp.com",
+  projectId: "capstone-db-54ca2",
+  storageBucket: "capstone-db-54ca2.appspot.com",
+  messagingSenderId: "17476986722",
+  appId: "1:17476986722:web:651a768b369193afb4ee3b"
+};
+
+const firebasApp = initializeApp(firebaseConfig);
