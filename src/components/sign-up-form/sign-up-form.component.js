a71593995c3c5db1ee274dc56f4ee2a201efc74f
Edgar <edgarsargsyan@hotmail.com>
2022-11-15 17:51:04 -0600

    Create a sign-up-form component and initalize form with four inputs and lebels

---
+++ src/components/sign-up-form/sign-up-form.component.js
@@ -0,0 +1,24 @@
+
+
+const SignUpForm = () => {
+
+  return (
+    <div>
+      <h1>Sign Up With your Email and Password</h1>
+      <form>
+        <label>Display Name</label>
+        <input required />
+
+        <label>Email</label>
+        <input required />
+
+        <label>Password</label>
+        <input required />
+
+        <label>Confirm Password</label>
+        <input required />
+      </form>
+    </div>
+
+  );
+}
