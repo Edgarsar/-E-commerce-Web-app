a01c87c4313c3dc4ed1b08fc9314a91e7850319a
Edgar <edgarsargsyan@hotmail.com>
2022-11-15 19:42:35 -0600

    Create a resetFields method to clear the form fields after form submission

--- src/components/sign-up-form/sign-up-form.component.js
+++ src/components/sign-up-form/sign-up-form.component.js
@@ -14,8 +14,9 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
-  console.log(formFields)
-
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -28,6 +29,7 @@ const SignUpForm = () => {
     try {
       const { user } = await createAuthUserWithEmailAndPassword(email, password);
       await createUserDocumentFromAuth(user, { displayName })
+      resetFormFields();
 
     }
     catch (error) {
