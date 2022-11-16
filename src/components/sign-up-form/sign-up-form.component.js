ca2ad11de89d02689e803bcdc17346fae77f5db7
Edgar <edgarsargsyan@hotmail.com>
2022-11-15 18:08:16 -0600

    Create a value field inside the inputs with the value of initial object fields

--- src/components/sign-up-form/sign-up-form.component.js
+++ src/components/sign-up-form/sign-up-form.component.js
@@ -11,21 +11,26 @@ const SignUpForm = () => {
   const [formFields, setFormField] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = defaultFormFields;
 
+  const handleChange = (event) => {
+
+  };
+
   return (
     <div>
       <h1>Sign Up With your Email and Password</h1>
       <form onSubmit={() => { }}>
         <label>Display Name</label>
-        <input type="text" required />
+        <input type="text" required onChange={handleChange} name="displayName" value={displayName} />
 
         <label>Email</label>
-        <input type="email" required />
+        <input type="email" required onChange={handleChange} name="email" value={email} />
 
         <label>Password</label>
-        <input type="password" required />
+        <input type="password" required onChange={handleChange} name="password" value={password} />
 
         <label>Confirm Password</label>
-        <input type="password" required />
+        <input type="password" required onChange={handleChange} nmae="confirmPassword" value={confirmPassword} />
+
         <button type="submit">Sign UP</button>
       </form>
     </div>
