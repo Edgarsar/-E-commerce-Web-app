ddf3ff85fe155dfd23e16dc03573b5b4067fb22a
Edgar <edgarsargsyan@hotmail.com>
2022-11-17 20:09:37 -0600

    Handle an error messages

--- src/components/sign-in-form/sign-in-form.component.js
+++ src/components/sign-in-form/sign-in-form.component.js
@@ -38,9 +38,17 @@ const SignInForm = () => {
       const response = await signInAuthUserWithEmailAndPassword(email, password);
       console.log(response)
       resetFormFields();
-    }
-    catch (error) {
-
+    } catch (error) {
+      switch (error.code) {
+        case 'auth/wrong-password':
+          alert('incorrect password for email');
+          break;
+        case 'auth/user-not-found':
+          alert('no user associated with this email');
+          break;
+        default:
+          console.log(error);
+      }
     }
   };
 
@@ -77,7 +85,7 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type='submit'>Sign In</Button>
-          <Button buttonType="google" onClick={signInWithGoogle}>Google sign in</Button>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle}>Google sign in</Button>
         </div>
 
       </form>
