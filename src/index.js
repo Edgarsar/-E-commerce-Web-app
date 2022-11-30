d9f0a629750bacfb2b3fd538aefeddaa5fcd7f42
Edgar <edgarsargsyan@hotmail.com>
2022-11-29 20:11:31 -0600

    Wrap App component with ProductsProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { UserProvider } from './components/contexts/user.context';
+import { ProductsProvider } from './components/contexts/products.context';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
@@ -11,7 +12,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
