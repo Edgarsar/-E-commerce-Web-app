d8e9abc7ce1d0e3e3cdc137cbca28714f940da1c
Edgar <edgarsargsyan@hotmail.com>
2022-12-08 15:05:39 -0600

    Retrieve a data from firestore

--- src/components/contexts/products.context.js
+++ src/components/contexts/products.context.js
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 
-import { addCollectionAndDocuments } from "../../utils/firebase/firebase.utils.js";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils.js";
 
 import SHOP_DATA from '../../shop-data.js';
 
@@ -11,9 +11,14 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
-    addCollectionAndDocuments("collection", SHOP_DATA);
-  }, [])
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    }
+    getCategoriesMap();
+  },[])
 
   const value = { products };
   return (
