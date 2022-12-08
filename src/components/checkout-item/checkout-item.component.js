2c401e7303b21168636565291ed52db591d8dde2
Edgar <edgarsargsyan@hotmail.com>
2022-12-07 18:27:04 -0600

    Implemet increment and decrement features

--- src/components/checkout-item/checkout-item.component.js
+++ src/components/checkout-item/checkout-item.component.js
@@ -4,10 +4,12 @@ import './checkout-item.styles.scss';
 
 const CheckoutItem = ({ cartItem }) => {
 
-  const { clearItemFromCart } = useContext(CartContext);
+  const { addItemToCart, removeItemToCart, clearItemFromCart } = useContext(CartContext);
   const { name, imageUrl, price, quantity } = cartItem;
 
   const cleaItemHandler = () => clearItemFromCart(cartItem);
+  const addItemHandler = () => addItemToCart(cartItem);
+  const removeCartHandler = () => removeItemToCart(cartItem);
 
   return (
     <div className='checkout-item-container'>
@@ -15,7 +17,17 @@ const CheckoutItem = ({ cartItem }) => {
         <img src={imageUrl} alt={`${name}`} />
       </div>
       <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
+      <span className='quantity'>
+        <div className='arrow' onClick={removeCartHandler}>
+          &#10094;
+        </div>
+        <span className='value'>
+          {quantity}
+        </span>
+        <div className='arrow' onClick={addItemHandler}>
+          &#10095;
+        </div>
+      </span>
       <span className='price'>{price}</span>
       <div className='remove-button' onClick={cleaItemHandler}>&#10005;</div>
     </div>
