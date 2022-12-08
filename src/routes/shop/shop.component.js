d2a6fda6f4042ffc64a1f3f6e30726f1726d1a64
Edgar <edgarsargsyan@hotmail.com>
2022-12-08 15:26:51 -0600

    Cahnge the product context to categories context

--- src/routes/shop/shop.component.js
+++ src/routes/shop/shop.component.js
@@ -1,19 +1,29 @@
-import { useContext } from "react";
+import { useContext, Fragment } from "react";
 
-import { ProductsContext } from "../../components/contexts/products.context";
+import { CategoriesContext } from "../../components/contexts/categories.context";
 
 import ProductCard from "../../components/product-card/product-card.component";
 
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext)
+  const { categoriesMap } = useContext(CategoriesContext)
   return (
-    <div className="products-container">
-      {products.map((product) => (
-        <ProductCard id={product.id} product={product}></ProductCard>
-      ))}
-    </div>
+    <Fragment>
+      {
+        Object.keys(categoriesMap).map((title) => (
+          <Fragment key={title}>
+            <h2>{title}</h2>
+            <div className="products-container">
+              {categoriesMap[title].map((product) => (
+                <ProductCard key={product.id} product={product}></ProductCard>
+              ))}
+            </div>
+          </Fragment>
+        ))
+      }
+
+    </Fragment>
   );
 };
 
