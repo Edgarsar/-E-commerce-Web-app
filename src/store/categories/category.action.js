a57f1ad0179f0e0e6ad58a740622c5421d9f1854
Edgar <edgarsargsyan@hotmail.com>
2022-12-30 17:59:09 -0600

    Updating thunk pattern for fetching categories

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,24 @@
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPE } from "./category.types";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGORIES_ACTION_TYPE.SET_CATEGORIES, categoriesArray);
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSucces = (categoriesArray) =>
+  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCES, categoriesArray);
+
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart());
+  try {
+    const categoriesArray = await getCategoriesAndDocuments();
+    dispatch(fetchCategoriesSucces(categoriesArray));
+  } catch (error) {
+    dispatch(fetchCategoriesFailed(error));
+  }
+};
