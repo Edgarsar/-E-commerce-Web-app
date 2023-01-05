91391bfd0232f3dce8b28b89ece5339dbb2d3860
Edgar <edgarsargsyan@hotmail.com>
2023-01-05 17:51:47 -0600

    Create a serverless function to handle a stripe request

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-exports.handler = async (event) {
+exports.handler = async (event) => {
   try {
     const { amount } = JSON.parse(event.body);
 
@@ -28,4 +28,4 @@ exports.handler = async (event) {
     };
   }
 
-}
+};
